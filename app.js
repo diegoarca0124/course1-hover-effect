26f092c36a0c962c931cabfe4a5d58e9710f0c9f
diegoarca0124 <155889956+diegoarca0124@users.noreply.github.com>
2024-05-03 14:37:34 -0500

    first-commit

---
+++ app.js
@@ -0,0 +1,39 @@
+new hoverEffect({
+    parent: document.getElementById('hover-1'),
+    intensity: 1,
+    speedIn: 0.5,
+    speedOut: 0.5,
+    displacementImage: './assets/displacement/pixels.webp',
+    image1: './assets/img/1.webp',
+    image2: './assets/img/2.webp',
+})
+
+new hoverEffect({
+    parent: document.getElementById('hover-2'),
+    intensity: 1,
+    speedIn: 0.5,
+    speedOut: 0.5,
+    displacementImage: './assets/displacement/fade.png',
+    image1: './assets/img/3.jpg',
+    image2: './assets/img/4.jpg',
+})
+
+new hoverEffect({
+    parent: document.getElementById('hover-3'),
+    intensity: 1,
+    speedIn: 0.5,
+    speedOut: 0.5,
+    displacementImage: './assets/displacement/water.jpg',
+    image1: './assets/img/5.jpg',
+    image2: './assets/img/6.jpg',
+})
+
+new hoverEffect({
+    parent: document.getElementById('hover-4'),
+    intensity: 1,
+    speedIn: 0.5,
+    speedOut: 0.5,
+    displacementImage: './assets/displacement/glass.jpg',
+    image1: './assets/img/7.jpg',
+    image2: './assets/img/8.jpg',
+})
